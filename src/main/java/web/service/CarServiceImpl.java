74c3cbcdb80cde62084007f1bc90dfd5a74822ce
DmitrySmmm <deein@bk.ru>
2024-06-07 13:36:55 +0300

    first commit

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,7 +1,9 @@
 package web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import web.model.Car;
+import web.repository.CarRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,25 +12,15 @@ import java.util.stream.Collectors;
 @Component
 public class CarServiceImpl implements CarService {
 
-    private List<Car> cars = new ArrayList<>();
-    {
-        cars.add(new Car("Volkswagen", "Golf", 1.8));
-        cars.add(new Car("Audi", "A6", 3.0));
-        cars.add(new Car("BMW", "3", 2.0));
-        cars.add(new Car("Seat","Freetrack",2.0));
-        cars.add(new Car("Skoda","Octavia",1.8));
-        cars.add(new Car("Mercedes","E-Klasse",3.5));
-        cars.add(new Car("Toyota","Corolla", 1.6));
-        cars.add(new Car("MINI","Countryman", 2.0));
-        cars.add(new Car("Porsche","Cayenne", 4.0));
-    }
+    @Autowired
+    CarRepository carRep;
 
     @Override
     public List<Car> showCar(Integer count) {
         if (count <= 4 && count >= 1) {
-            return cars.stream().limit(count).collect(Collectors.toList());
+            return carRep.getCars().stream().limit(count).collect(Collectors.toList());
         } else {
-            return cars;
+            return carRep.getCars();
         }
     }
 }
